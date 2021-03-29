927b7a4ff13af3ff2a2ef3cb4e6c7517cd4a97d3
berkctezc <berkcantezcaner@gmail.com>
2021-03-29 14:45:02 +0300

    created some entitites classes and interfaces

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    //diger katmanlardan ulasılması icin public
+    //default:internal=sadece bu proje erisebilir (entitites)
+    public class Product 
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
