c6d61fadc23ad3358a477a7f6d319ba26ea0dad1
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-01 02:43:58 +0300

    ef added to dal

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -8,7 +9,12 @@ using System.Threading.Tasks;
 namespace DataAccess.Abstract
 {
     //tekrarlayan interfaceleri ortak bir interface icerisine yerlestirdik
-    public interface IEntityRepository<T>
+
+    //generic constraint
+    //class: referans tip olabilir anlamına gelmekte!
+    //IEntity: IEntity veya IEntity implement eden bir nesne olabilir
+    //new(): newlenebilir olmalı (IEntity olamayacak)
+    public interface IEntityRepository<T> where T: IEntity, new() //class yazmak hata veriyor
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
         T Get(Expression<Func<T, bool>> filter);
