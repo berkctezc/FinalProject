6ef4282f57bb1c1fb68008fe07d6d2bd681ccceb
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-23 21:15:21 +0300

    thread sleep added

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -28,6 +29,8 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+
+            Thread.Sleep(500);
             //Dependency chain -->  IProductService productService = new ProductManager(new EfProductDal()); 
             var result = _productService.GetAll();
             if (result.Success)
