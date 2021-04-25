16f2341160efb25c50fc0a2f2f37eb58fb102d93
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-25 18:30:22 +0300

    mar13 final commit

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Business.Abstract;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -33,6 +33,7 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }        
         
+
         [HttpGet("getbyid")]
         public IActionResult GetById(int id)
         {
@@ -43,6 +44,29 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getproductdetails")]
+        public IActionResult GetProductDetails(int categoryId)
+        {
+            var result = _productService.GetProductDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
 
         [HttpPost("add")]
         public IActionResult Add(Product product)
