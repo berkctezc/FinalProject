307ff5bab9eaa4a87084a842103072029101c0e0
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-28 16:10:20 +0300

    mar20 final commit

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using FluentValidation.Results;
+using Newtonsoft.Json;
 
 namespace Core.Extensions
 {
@@ -11,5 +13,10 @@ namespace Core.Extensions
         {
             return JsonConvert.SerializeObject(this);
         }
+    }
+
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> Errors { get; set; }
     }
 }
--- Core/Extensions/ExceptionMiddleware.cs
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
 
 namespace Core.Extensions
@@ -33,9 +35,19 @@ namespace Core.Extensions
             httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             string message = "Internal Server Error";
+            IEnumerable<ValidationFailure> errors;
             if (e.GetType() == typeof(ValidationException))
             {
                 message = e.Message;
+                errors = ((ValidationException) e).Errors;
+                httpContext.Response.StatusCode = 400;
+
+                return httpContext.Response.WriteAsync(new ValidationErrorDetails
+                {
+                    StatusCode = 400,
+                    Message = message,
+                    Errors=errors
+                }.ToString());
             }
 
             return httpContext.Response.WriteAsync(new ErrorDetails
